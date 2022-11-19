b7501a3c844eaa393a1322756941cacd29c7d3e6
atchaya1617 <113231311+atchaya1617@users.noreply.github.com>
2022-11-19 22:30:24 +0530

    Update app.py

--- Project Development Phase/Sprint2/app.py
+++ Project Development Phase/Sprint2/app.py
@@ -1,57 +1,42 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, redirect, url_for
+import requests
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def index():
-    return render_template('signin.html')
+def init():
+    return redirect(url_for('home'))
 
-@app.route('/signup')
-def signup_form():
-    return render_template('signup.html')
 
 @app.route('/home')
 def home():
-    return render_template('home.html')
-
-@app.route('/headlines')
-def headlines():
-    return render_template('notifications.html')
-
-@app.route('/articles')
-def articles():
-    return render_template('profile.html')
-
-@app.route('/sources')
-def sources():
-    return render_template('sources.html')
+    return render_template("home.html")
 
 
-@app.route('/category/business')
-def business():
-    return  render_template('business.html', sources = sources)
+@app.route('/index1')
+def index():
+        url='https://newsapi.org/v2/top-headlines?country=in&apiKey=0b847d15f1ce42c9a341daa9b3369ad4'
+        r=requests.get(url).json()
+        cases={
+        'articles' : r['articles']
+            }
+        return render_template("index1.html",case= cases)
 
-@app.route('/category/tech')
-def tech():
-    return  render_template('tech.html', sources = sources)
+@app.route('/login')
+def login():
+    error=1
+    return render_template("login.html")
 
-@app.route('/category/entertainment')
-def entertainment():
-    return  render_template('entertainment.html', sources = sources)
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
-@app.route('/category/science')
-def science():
-    return  render_template('science.html', sources = sources)
 
-@app.route('/category/sports')
-def sports():
-    return  render_template('sport.html', sources = sources)
+@app.route('/logout')
+def logout():
+    return render_template('logout.html') 
 
-@app.route('/category/health')
-def health():
-    return  render_template('health.html', sources = sources)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
+   
